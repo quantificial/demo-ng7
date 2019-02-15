2fca43ca9b4329787545688b8bc5f9e5854ff8ef
Johnson Fu <johnson.fu@chubb.com>
2019-02-15 13:38:24 +0800

    update

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ package-lock.json - record others dependencies version
 
 # https://coursetro.com/posts/code/171/Angular-7-Tutorial---Learn-Angular-7-by-Example
 
+Angular Components
+- HTML Template
+- The Logic in the typescript
+- Styling in SCSS and CSS
+
+# define route
+- app-routeing.module.ts
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  users: Object;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('https://reqres.in/api/users').subscribe(data => {
+      console.log(data);
+      this.users = data;
+    });
+
   }
 
 }
